b5959db011e1bd9502b9088bb6c162459d27be72
SayFu11a <sgishlakaev@mail.ru>
2024-06-12 18:39:49 +0300

    deploy 5

--- index.js
+++ index.js
@@ -70,31 +70,9 @@ app.patch(
 app.post('/booking', BookingController.handleBooking);
 app.get('/booking', BookingController.getAll);
 
-app.listen(process.env.PORT || 4444, (err) => {
+app.listen(4444, (err) => {
    if (err) {
       return console.log(err);
    }
-
    console.log('Server OK');
 });
-
-// app.get('/', (req, res) => {
-//    res.send('Hello World');
-// }); // если придет гет запрос вернуть хеловорлд
-
-// app.post('/auth/login', (req, res) => {
-//    console.log(req.body); // с помошью req получаем инфу
-
-//    const token = jwt.sign(
-//       {
-//          email: req.body.email,
-//          fullName: 'Хрен Моржовый',
-//       },
-//       'secret123',
-//    ); // шифрует данные
-
-//    res.json({
-//       success: true,
-//       token,
-//    });
-// }); //  если придет пост запрос вернуть сексес тру
